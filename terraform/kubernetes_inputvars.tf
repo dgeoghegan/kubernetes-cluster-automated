67de021b233672397cf328fa2f143a1a261aeb92
dgeoghegan <dmgeoghegan@gmail.com>
2025-01-31 14:53:06 -0500

    add locals and ansible_output_vars, modify inputvars

--- terraform/kubernetes_inputvars.tf
+++ terraform/kubernetes_inputvars.tf
@@ -6,3 +6,8 @@ variable "kubernetes_cidrblock_start" {
 
 }
 
+variable "ansible_file_path_override" {
+  type = string
+  default = ""
+  description = "Full path for files created for ansible. Defaults to $${path.module}/../ansible/files_from_terraform"
+}
---
+++ terraform/locals.tf
@@ -0,0 +1,24 @@
+locals {
+  kubernetes_worker_network_info = [
+    for idx in range(length(aws_instance.kubernetes_worker)) : {
+      private_dns = aws_instance.kubernetes_worker[idx].private_dns
+      private_ip  = aws_instance.kubernetes_worker[idx].private_ip
+      public_ip   = aws_instance.kubernetes_worker[idx].public_ip
+      name        = aws_instance.kubernetes_worker[idx].tags["Name"]
+    }
+  ]
+  
+  kubernetes_controller_network_info = [
+    for idx in range(length(aws_instance.kubernetes_controller)) : {
+      private_dns = aws_instance.kubernetes_controller[idx].private_dns
+      private_ip  = aws_instance.kubernetes_controller[idx].private_ip
+      public_ip   = aws_instance.kubernetes_controller[idx].public_ip
+      name        = aws_instance.kubernetes_controller[idx].tags["Name"]
+    }
+  ]
+
+### OUTPUT LOCATION FOR ANSIBLE FILES (CERTS, HOSTS, ETC.) ####
+  ansible_file_path_default = "${path.module}/../ansible/files_from_terraform"
+  ansible_file_path = length(var.ansible_file_path_override) > 0 ? var.ansible_file_path_override : local.ansible_file_path_default
+
+}
