efa4d76e50e50ee26505f20639aa8b8029dbe201
dgeoghegan <dmgeoghegan@gmail.com>
2025-02-03 11:06:18 -0500

    update providers and lock file

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -11,5 +11,13 @@ terraform {
     local = {
       source = "hashicorp/local"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.3"
+    }
   }
 }
