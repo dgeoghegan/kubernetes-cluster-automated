e7957ded552a28a79d3b6f06151126b6a165e6d3
dgeoghegan <dmgeoghegan@gmail.com>
2025-03-26 13:17:34 -0400

    k8s locals: make ready for cloud-type, disable local files

--- terraform/modules/kubernetes/locals.tf
+++ terraform/modules/kubernetes/locals.tf
@@ -1,25 +1,29 @@
 locals {
-  kubernetes_worker_network_info = [
+  kubernetes_worker_private_dns = var.cloud_type == "aws" ? [for instance in aws_instance.kubernetes_worker : instance.private_dns] : []
+  kubernetes_worker_private_ip  = var.cloud_type == "aws" ? [for instance in aws_instance.kubernetes_worker : instance.private_ip] : []
+  kubernetes_worker_public_ip   = var.cloud_type == "aws" ? [for instance in aws_instance.kubernetes_worker : instance.public_ip] : []
+
+  kubernetes_worker_network_info = var.cloud_type == "aws" ? [ 
     for idx in range(length(aws_instance.kubernetes_worker)) : {
       private_dns = aws_instance.kubernetes_worker[idx].private_dns
       private_ip  = aws_instance.kubernetes_worker[idx].private_ip
       public_ip   = aws_instance.kubernetes_worker[idx].public_ip
       name        = aws_instance.kubernetes_worker[idx].tags["Name"]
     }
-  ]
+  ] : []
 
-  kubernetes_worker_host_entries = [
+  kubernetes_worker_host_entries = var.cloud_type == "aws" ? [
     for worker in local.kubernetes_worker_network_info : "${worker.private_ip} ${split(".", worker.private_dns)[0]}"
-  ]
+  ] : []
   
-  kubernetes_controller_network_info = [
+  kubernetes_controller_network_info = var.cloud_type == "aws"? [
     for idx in range(length(aws_instance.kubernetes_controller)) : {
       private_dns = aws_instance.kubernetes_controller[idx].private_dns
       private_ip  = aws_instance.kubernetes_controller[idx].private_ip
       public_ip   = aws_instance.kubernetes_controller[idx].public_ip
       name        = aws_instance.kubernetes_controller[idx].tags["Name"]
     }
-  ]
+  ] : []
 
   kubernetes_initial_cluster = join(",", [
     for idx, controller in local.kubernetes_controller_network_info : 
@@ -27,7 +31,7 @@ locals {
   ])
 
 ### OUTPUT LOCATION FOR ANSIBLE FILES (CERTS, HOSTS, ETC.) ####
-  ansible_file_path_default = "${path.module}/../ansible/files_from_terraform"
-  ansible_file_path = length(var.ansible_file_path_override) > 0 ? var.ansible_file_path_override : local.ansible_file_path_default
+#  ansible_file_path_default = "${path.module}/../ansible/files_from_terraform"
+#  ansible_file_path = length(var.ansible_file_path_override) > 0 ? var.ansible_file_path_override : local.ansible_file_path_default
 
 }
