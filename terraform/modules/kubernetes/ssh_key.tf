5c58a4bb47565e30c745bbd5029a3caed47fce82
dgeoghegan <dmgeoghegan@gmail.com>
2025-03-26 13:14:48 -0400

    k8s ssh: add cluster to ssh key name

--- terraform/modules/kubernetes/ssh_key.tf
+++ terraform/modules/kubernetes/ssh_key.tf
@@ -4,14 +4,7 @@ resource "tls_private_key" "kubernetes_ssh_key" {
   rsa_bits  = 4096
 }
 
-resource "local_file" "kubernetes_ssh_key" {
-  filename = "${local.ansible_file_path}/kubernetes_ssh_key"
-  content = tls_private_key.kubernetes_ssh_key.private_key_openssh
-  file_permission = "0400"
-}
-
-
 resource "aws_key_pair" "kubernetes_ssh_key" {
-  key_name    = "kubernetes_ssh_key"
+  key_name    = "kubernetes_ssh_key-${var.cluster_index}"
   public_key  = tls_private_key.kubernetes_ssh_key.public_key_openssh
 }
