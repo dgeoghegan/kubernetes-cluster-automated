2518a958892c3549839fcd67b5c27d4e268b7a99
dmgeoghegan <dmgeoghegan@gmail.com>
2025-11-04 12:22:00 -0500

    sample environment

---
+++ terraform/envs/tier-sample/cluster-sample/input_vars.tf
@@ -0,0 +1,53 @@
+variable "cloud_type" {
+  type        = string
+  description = "The cloud provider to use (e.g., aws, gcp, azure)."
+  default     = "aws"
+}
+
+variable "controller_max" {
+  type        = number
+  description = "The maximum number of Kubernetes control plane instances to create."
+  default     = 3
+}
+
+variable "worker_max" {
+  type        = number
+  description = "The maximum number of Kubernetes worker nodes to create."
+  default     = 3
+}
+
+variable "ha_enabled" {
+  type        = bool
+  description = "Whether to enable high availability mode for the Kubernetes control plane."
+  default     = true
+}
+
+variable "network_name" {
+  type        = string
+  description = "A base name to use for network-related resources (VPCs, security groups, etc)."
+  default     = "kubernetes-cluster"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The instance type for the controllers and workers"
+  default     = "t3.micro"
+}
+
+variable "backend_type" {
+  type        = string
+  description = "The storage backend to use (currently just 's3')"
+  default     = "s3"
+}
+
+variable "cluster_index" {
+  type        = number
+  description = "A unique index for this cluster (0, 1, 2, etc.), used to derive network names and CIDRs."
+  default     = 0
+}
+
+variable "aws_credentials_file" {
+  description = "Path to an AWS credentials file"
+  type        = string
+  default     = "./aws_credentials.ini"
+}
---
+++ terraform/envs/tier-sample/cluster-sample/main.tf
@@ -0,0 +1,72 @@
+locals {
+  service_cidr  = cidrsubnet("192.168.0.0/16", 6, var.cluster_index)
+  storage_name  = "k8s-cluster-${var.cluster_index}-${substr(md5(var.network_name), 0, 6)}"
+  default_credentials_file = "$(path.module)/aws_credentials.tf"
+}
+
+module "network" {
+  source         = "../../../modules/network/"
+  cloud_type     = var.cloud_type
+  network_name   = var.network_name
+  cluster_index  = var.cluster_index
+  service_cidr   = local.service_cidr
+  controller_max = var.controller_max
+  worker_max     = var.worker_max
+  ha_enabled     = var.ha_enabled
+}
+
+module "storage" {
+  source        = "../../../modules/storage"
+  backend_type  = var.backend_type
+  storage_name  = local.storage_name
+  files         = module.kubernetes.flattened_kubernetes_file_contents
+  depends_on    = [module.kubernetes]
+}
+
+module "certificate_authority" {
+  source       = "../../../modules/certificate_authority"
+  network_name = var.network_name
+}
+
+module "kubernetes" {
+  source                 = "../../../modules/kubernetes"
+  cloud_type             = var.cloud_type
+  controller_max         = var.controller_max
+  worker_max             = var.worker_max
+  ha_enabled             = var.ha_enabled
+  instance_type          = var.instance_type
+  load_balancer_dns_name = module.network.load_balancer_dns_name
+  vpc_cidr               = module.network.vpc_cidr
+  cluster_index          = var.cluster_index
+  service_cidr           = local.service_cidr
+  subnet                 = module.network.subnet
+  security_group_id      = module.network.security_group_id
+  private_key_pem        = module.certificate_authority.private_key_pem
+  cert_pem               = module.certificate_authority.cert_pem
+  load_balancer_listener_port = module.network.load_balancer_listener_port
+}
+
+module "security" {
+  source                 = "../../../modules/security"
+  cloud_type             = var.cloud_type
+  backend_type           = var.backend_type
+  storage_name           = local.storage_name
+  storage_id             = module.storage.storage_id
+  depends_on             = [module.storage]
+}
+
+module "docker" {
+  source                = "../../../modules/docker"
+  cloud_type            = var.cloud_type
+  cluster_index         = var.cluster_index
+  storage_name          = local.storage_name
+  region                = null
+  docker_instance_type  = var.instance_type
+  vpc_cidr              = module.network.vpc_cidr
+  subnet                = module.network.subnet
+  security_group_id     = module.network.security_group_id
+  profile_id            = module.security.profile_id
+  depends_on            = [module.storage]
+  vpc_id                = module.network.vpc_id
+  k8s_files_hash        = module.kubernetes.k8s_files_hash
+}
