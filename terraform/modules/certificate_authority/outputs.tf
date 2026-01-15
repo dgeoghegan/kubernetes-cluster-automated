9a7e37ecca6bcee9ed66b19e978789ba19c343b4
dmgeoghegan <dmgeoghegan@gmail.com>
2026-01-15 09:56:00 -0500

    minor spacing issue

--- terraform/modules/certificate_authority/outputs.tf
+++ terraform/modules/certificate_authority/outputs.tf
@@ -1,7 +1,9 @@
 output "cert_pem" {
-  value = tls_self_signed_cert.ca.cert_pem
+  value     = tls_self_signed_cert.ca.cert_pem
+  sensitive = true
 }
 
 output "private_key_pem" {
-  value = tls_private_key.ca.private_key_pem
+  value     = tls_private_key.ca.private_key_pem
+  sensitive = true
 }
