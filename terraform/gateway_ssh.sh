2af52e71791bce923a374c5d66194ad22682bf40
dmgeoghegan <dmgeoghegan@gmail.com>
2026-01-15 10:29:18 -0500

    remove disabled files under terraform

--- terraform/gateway_ssh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh ubuntu@$(terraform output | grep "ssh_gateway" | cut -d "\"" -f 2) -i $(cat aws_credentials.tf | grep ssh_gateway -A 1 | grep public_key | cut -f 2 -d "\"" | sed 's/\.pub//')
